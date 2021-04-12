6027b56d00abb920c0db4b767c5b66771b79fab9
XHDYC <51146504+XHDYC@users.noreply.github.com>
2021-04-12 08:57:40 +0800

    Update pose_resnet.py

--- lib/models/pose_resnet.py
+++ lib/models/pose_resnet.py
@@ -14,7 +14,7 @@ import logging
 import torch
 import torch.nn as nn
 from collections import OrderedDict
-
+from functools import reduce
 
 BN_MOMENTUM = 0.1
 logger = logging.getLogger(__name__)
@@ -24,121 +24,83 @@ def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
+#sk层
+class SKConv(nn.Module):
+    def __init__(self,in_channels,out_channels,stride=1,M=2,r=16,L=32):
+        '''
+        :param in_channels:  输入通道维度
+        :param out_channels: 输出通道维度   原论文中 输入输出通道维度相同
+        :param stride:  步长，默认为1
+        :param M:  分支数
+        :param r: 特征Z的长度，计算其维度d时所需的比率（论文中 特征S->Z 是降维，故需要规定 降维的下界）
+        :param L:  论文中规定特征Z的下界，默认为32
+        '''
+        super(SKConv,self).__init__()
+        d=max(in_channels//r,L)   # 计算向量Z 的长度d
+        self.M=M
+        self.out_channels=out_channels
+        self.conv=nn.ModuleList()  # 根据分支数量 添加 不同核的卷积操作
+        for i in range(M):
+            # 为提高效率，原论文中 扩张卷积5x5为 （3X3，dilation=2）来代替。 且论文中建议组卷积G=32
+            self.conv.append(nn.Sequential(nn.Conv2d(in_channels,out_channels,3,stride,padding=1+i,dilation=1+i,groups=32,bias=False),
+                                           nn.BatchNorm2d(out_channels),
+                                           nn.ReLU(inplace=True)))
+        self.global_pool=nn.AdaptiveAvgPool2d(1) # 自适应pool到指定维度    这里指定为1，实现 GAP
+        self.fc1=nn.Sequential(nn.Conv2d(out_channels,d,1,bias=False),
+                               nn.BatchNorm2d(d),
+                               nn.ReLU(inplace=True))   # 降维
+        self.fc2=nn.Conv2d(d,out_channels*M,1,1,bias=False)  # 升维
+        self.softmax=nn.Softmax(dim=1) # 指定dim=1  使得两个全连接层对应位置进行softmax,保证 对应位置a+b+..=1
+    def forward(self, input):
+        batch_size=input.size(0)
+        output=[]
+        #the part of split
+        for i,conv in enumerate(self.conv):
+            #print(i,conv(input).size())
+            output.append(conv(input))
+        #the part of fusion
+        U=reduce(lambda x,y:x+y,output) # 逐元素相加生成 混合特征U
+        s=self.global_pool(U)
+        z=self.fc1(s)  # S->Z降维
+        a_b=self.fc2(z) # Z->a，b 升维  论文使用conv 1x1表示全连接。结果中前一半通道值为a,后一半为b
 
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
+        a_b=a_b.reshape(batch_size,self.M,self.out_channels,-1) #调整形状，变为 两个全连接层的值
+        a_b=self.softmax(a_b) # 使得两个全连接层对应位置进行softmax
+        #the part of selection
+        a_b=list(a_b.chunk(self.M,dim=1))#split to a and b   chunk为pytorch方法，将tensor按照指定维度切分成 几个tensor块
+        a_b=list(map(lambda x:x.reshape(batch_size,self.out_channels,1,1),a_b)) # 将所有分块  调整形状，即扩展两维
+        V=list(map(lambda x,y:x*y,output,a_b)) # 权重与对应  不同卷积核输出的U 逐元素相乘
+        V=reduce(lambda x,y:x+y,V) # 两个加权后的特征 逐元素相加
+        return V
+#改bottleneck:
 class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1,
-                               bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion,
-                                  momentum=BN_MOMENTUM)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
+    '''
+    基于Res Block构造的SK Block
+    ResNeXt有  1x1Conv（通道数：x） +  SKConv（通道数：x）  + 1x1Conv（通道数：2x） 构成
+    '''
+    expansion=2 #指 每个block中 通道数增大指定倍数
+    def __init__(self,inplanes,planes,stride=1,downsample=None):
+        super(Bottleneck,self).__init__()
+        self.conv1=nn.Sequential(nn.Conv2d(inplanes,planes,1,1,0,bias=False),
+                                 nn.BatchNorm2d(planes),
+                                 nn.ReLU(inplace=True))
+        self.conv2=SKConv(planes,planes,stride)
+        self.conv3=nn.Sequential(nn.Conv2d(planes,planes*self.expansion,1,1,0,bias=False),
+                                 nn.BatchNorm2d(planes*self.expansion))
+        self.relu=nn.ReLU(inplace=True)
+        self.downsample=downsample
+    def forward(self, input):
+        shortcut=input
+        output=self.conv1(input)
+        output=self.conv2(output)
+        output=self.conv3(output)
         if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
+            shortcut=self.downsample(input)
+        output+=shortcut
+        return self.relu(output)
 
 
-class Bottleneck_CAFFE(nn.Module):
-    expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck_CAFFE, self).__init__()
-        # add stride to conv1x1
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1,
-                               bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion,
-                                  momentum=BN_MOMENTUM)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
 
 
 class PoseResNet(nn.Module):
@@ -154,11 +116,11 @@ class PoseResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(64, momentum=BN_MOMENTUM)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-
+        self.layer1 = self._make_layer(block, 128, layers[0])
+        self.layer2 = self._make_layer(block, 256, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 512, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 1024, layers[3], stride=2)
+        
         # used for deconv layers
         self.deconv_layers = self._make_deconv_layer(
             extra.NUM_DECONV_LAYERS,
@@ -298,11 +260,27 @@ class PoseResNet(nn.Module):
             raise ValueError('imagenet pretrained model does not exist')
 
 
-resnet_spec = {18: (BasicBlock, [2, 2, 2, 2]),
-               34: (BasicBlock, [3, 4, 6, 3]),
+resnet_spec = {
                50: (Bottleneck, [3, 4, 6, 3]),
                101: (Bottleneck, [3, 4, 23, 3]),
                152: (Bottleneck, [3, 8, 36, 3])}
+
+
+def get_pose_net(cfg, is_train, **kwargs):
+    num_layers = cfg.MODEL.EXTRA.NUM_LAYERS
+    style = cfg.MODEL.STYLE
+
+    block_class, layers = resnet_spec[num_layers]
+
+    if style == 'caffe':
+        block_class = Bottleneck_CAFFE
+
+    model = PoseResNet(block_class, layers, cfg, **kwargs)
+
+    if is_train and cfg.MODEL.INIT_WEIGHTS:
+        model.init_weights(cfg.MODEL.PRETRAINED)
+
+    return model
 
 
 def get_pose_net(cfg, is_train, **kwargs):
